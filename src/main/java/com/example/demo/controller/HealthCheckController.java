725b1cad6264870ebf24f3c412a6a8b188877e73
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-01-03 16:32:31 +0900

    Feat: `GET /` API 추가

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HealthCheckController {
-
 
 	@GetMapping("/health")
 	public ResponseEntity<String> healthCheck() {
 		return ResponseEntity.ok("success");
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> hi() {
+		return ResponseEntity.ok("hello");
 	}
 
 }
